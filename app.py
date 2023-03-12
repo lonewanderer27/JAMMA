2f00ddb4b84922ad229076a5aff154c513d21ddd
lonewanderer27 <adrianejames27@gmail.com>
2023-03-13 02:13:40 +0800

    move secret_key up at the top

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from flask import Flask, redirect, render_template, request, session, abort, jsonify, url_for
-import os, time
+import os
 import firebase_admin
 from firebase_admin import credentials, db, storage
 from PIL import Image
 from werkzeug.utils import secure_filename
 from datetime import datetime
 import logging
-import json
-from operator import itemgetter  
+from operator import itemgetter
 
 print("Initializing Firebase Admin SDK...")
 
-cred_dict = {   
+cred_dict = {
     "type": os.environ.get("GOOGLE_PROJECT_TYPE"),
     "project_id": os.environ.get("GOOGLE_PROJECT_ID"),
     "private_key_id": os.environ.get("GOOGLE_PROJECT_PRIVATE_KEY_ID"),
@@ -33,19 +32,19 @@ firebase_admin.initialize_app(cred, {
     'storageBucket': 'jamma-comments-332612.appspot.com'
 })
 
-app = Flask(__name__)
+app = Flask(__name__, secret_key=os.urandom(12))
 app.config.update(
-    UPLOAD_FOLDER = 'static/temp',
-    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'},
-    TESTING = True,
+    UPLOAD_FOLDER='static/temp',
+    ALLOWED_EXTENSIONS={'png', 'jpg', 'jpeg', 'gif'},
+    TESTING=True,
 )
 
-#for load testing purposes
+# for load testing purposes
+
+
 @app.route("/loaderio-8f82c94de57fbc8606728068c3bba183/", methods=['GET'])
 def loaderio():
     return "loaderio-8f82c94de57fbc8606728068c3bba183"
-
-
 
 
 @app.route("/", methods=['GET'])
@@ -53,18 +52,18 @@ def home():
     if session.get('logged_in'):
         username = session.get('lastuser')
         profile_url = session.get('profile_url')
-        jammaLink = "https://jammacomments.herokuapp.com?"+"username="+username+"&profile_url="+profile_url
+        jammaLink = "https://jammacomments.herokuapp.com?" + \
+            "username="+username+"&profile_url="+profile_url
         active_category = 'featured'
         print(jammaLink)
         return render_template(
-            "index.html", 
-            jammaLink=jammaLink, 
-            username=username, 
-            profile_url=profile_url, 
+            "index.html",
+            jammaLink=jammaLink,
+            username=username,
+            profile_url=profile_url,
             active_category=active_category)
     else:
         return redirect(url_for('login'))
-
 
 
 @app.route("/index", methods=['GET'])
@@ -73,7 +72,8 @@ def index():
         username = session.get('lastuser')
         profile_url = session.get('profile_url')
         usermail = session.get('usermail')
-        jammaLink = "https://jammacomments.herokuapp.com/?"+"username="+username+"&profile_url="+profile_url
+        jammaLink = "https://jammacomments.herokuapp.com/?" + \
+            "username="+username+"&profile_url="+profile_url
         active_category = 'featured'
 
         ref = db.reference('/products')
@@ -84,26 +84,25 @@ def index():
         productsSales = ref.get()
         sorted_productsSales = {}
         counter = 0
-        for key, value in sorted(productsSales.items(), key = itemgetter(1), reverse = True):
+        for key, value in sorted(productsSales.items(), key=itemgetter(1), reverse=True):
             counter += 1
             if counter < 6:
                 sorted_productsSales[key] = value
-        
+
         return render_template(
-            "index.html", 
-        jammaLink=jammaLink, 
-        username=username, 
-        profile_url=profile_url, 
-        usermail=usermail,
-        active_category=active_category,
-        new_products=new_products,
-        products=products,
-        sorted_productsSales=sorted_productsSales,
+            "index.html",
+            jammaLink=jammaLink,
+            username=username,
+            profile_url=profile_url,
+            usermail=usermail,
+            active_category=active_category,
+            new_products=new_products,
+            products=products,
+            sorted_productsSales=sorted_productsSales,
         )
-        
+
     else:
         return redirect(url_for('login'))
-
 
 
 @app.route("/allproducts", methods=['GET', 'POST'])
@@ -114,23 +113,23 @@ def allproducts():
         username = session.get('lastuser')
         profile_url = session.get('profile_url')
         usermail = session.get('usermail')
-        jammaLink = "https://jammacomments.herokuapp.com/?"+"username="+username+"&profile_url="+profile_url
+        jammaLink = "https://jammacomments.herokuapp.com/?" + \
+            "username="+username+"&profile_url="+profile_url
         active_category = 'allproducts'
         ref = db.reference('/products')
         products = ref.get()
         return render_template(
-            "allproducts.html", 
-            jammaLink=jammaLink, 
-            username=username, 
+            "allproducts.html",
+            jammaLink=jammaLink,
+            username=username,
             usermail=usermail,
-            profile_url=profile_url, 
+            profile_url=profile_url,
             active_category=active_category,
             products=products,
-            )
+        )
 
     else:
         return redirect(url_for('login'))
-    
 
 
 @app.route("/smartwatch", methods=['GET', 'POST'])
@@ -141,28 +140,27 @@ def smartwatch():
         username = session.get('lastuser')
         profile_url = session.get('profile_url')
         usermail = session.get('usermail')
-        jammaLink = "https://jammacomments.herokuapp.com/?"+"username="+username+"&profile_url="+profile_url
+        jammaLink = "https://jammacomments.herokuapp.com/?" + \
+            "username="+username+"&profile_url="+profile_url
         active_category = 'smartwatch'
         ref = db.reference('/products')
         products = ref.get()
 
-        #Removes non-earphone products in products dictionary
+        # Removes non-earphone products in products dictionary
         products = filterproducts(products, 'smartwatch')
 
         return render_template(
-            "smartwatch.html", 
-            jammaLink=jammaLink, 
-            username=username, 
+            "smartwatch.html",
+            jammaLink=jammaLink,
+            username=username,
             usermail=usermail,
-            profile_url=profile_url, 
+            profile_url=profile_url,
             active_category=active_category,
             products=products,
-            )
+        )
 
     else:
         return redirect(url_for('login'))
-
-
 
 
 def filterproducts(products, category):
@@ -172,8 +170,6 @@ def filterproducts(products, category):
             products_temp[product] = products[product]
     products = products_temp
     return products
-        
-
 
 
 @app.route("/earphone", methods=['GET', 'POST'])
@@ -184,27 +180,27 @@ def earphone():
         username = session.get('lastuser')
         profile_url = session.get('profile_url')
         usermail = session.get('usermail')
-        jammaLink = "https://jammacomments.herokuapp.com/?"+"username="+username+"&profile_url="+profile_url
+        jammaLink = "https://jammacomments.herokuapp.com/?" + \
+            "username="+username+"&profile_url="+profile_url
         active_category = 'earphone'
         ref = db.reference('/products')
         products = ref.get()
 
-        #Removes non-earphone products in products dictionary, that is going to be passed to the html template
+        # Removes non-earphone products in products dictionary, that is going to be passed to the html template
         products = filterproducts(products, 'earphone')
 
         return render_template(
-            "earphone.html", 
-            jammaLink=jammaLink, 
-            username=username, 
+            "earphone.html",
+            jammaLink=jammaLink,
+            username=username,
             usermail=usermail,
-            profile_url=profile_url, 
+            profile_url=profile_url,
             active_category=active_category,
             products=products,
-            )
+        )
 
     else:
         return redirect(url_for('login'))
-
 
 
 @app.route("/about", methods=['GET', 'POST'])
@@ -215,19 +211,19 @@ def about():
         username = session.get('lastuser')
         profile_url = session.get('profile_url')
         usermail = session.get('usermail')
-        jammaLink = "https://jammacomments.herokuapp.com/?"+"username="+username+"&profile_url="+profile_url
+        jammaLink = "https://jammacomments.herokuapp.com/?" + \
+            "username="+username+"&profile_url="+profile_url
         active_category = 'about'
         return render_template(
-            "about.html", 
-            jammaLink=jammaLink, 
+            "about.html",
+            jammaLink=jammaLink,
             username=username,
             usermail=usermail,
-            profile_url=profile_url, 
+            profile_url=profile_url,
             active_category=active_category)
 
     else:
         return redirect(url_for('login'))
-
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -251,30 +247,31 @@ def login():
                     session['usermail'] = userAccounts[esername]['usermail']
 
                     visitor_ip_address = request.remote_addr
-                    logging.info(f"LOGIN SUCCESS    username: {esername}   password: {eserpass}    IP Address: {visitor_ip_address}")
+                    logging.info(
+                        f"LOGIN SUCCESS    username: {esername}   password: {eserpass}    IP Address: {visitor_ip_address}")
 
                     return redirect(url_for('index'))
 
-                else: 
+                else:
                     error = 'Incorrect Password'
-                    logging.warning(f"INCORRECT PASSWORD    username: {esername}   password: {eserpass}")
+                    logging.warning(
+                        f"INCORRECT PASSWORD    username: {esername}   password: {eserpass}")
                     return render_template("login.html", error=error)
 
             else:
                 error = 'User does not exist'
-                logging.warning(f"USER DOES NOT EXIST    username: {esername}   password: {eserpass}")
+                logging.warning(
+                    f"USER DOES NOT EXIST    username: {esername}   password: {eserpass}")
                 return render_template("login.html", error=error)
 
         return render_template(
             "login.html", lastuser=lastuser, message=message, message_minor=message_minor)
-        
-
 
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
-
+           filename.rsplit('.', 1)[1].lower(
+           ) in app.config['ALLOWED_EXTENSIONS']
 
 
 @app.route("/preregister")
@@ -286,7 +283,6 @@ def preregister():
         return redirect(url_for('register'))
 
 
-
 def empty_profile_url():
     bucket = storage.bucket()
     blob = bucket.blob("userpictures/Default_Profile.png")
@@ -295,12 +291,10 @@ def empty_profile_url():
     return profile_url
 
 
-
 def currentDateTime():
     now = datetime.now()
     datetime_complete_string = now.strftime("%d/%m/%Y %H:%M:%S:%f")
     return datetime_complete_string
-
 
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -317,31 +311,33 @@ def register():
             eserpass2 = request.form['userpass2']
             esertel = request.form['usertel']
 
-
             if 'userpicture' in request.files:
                 eserpicture = request.files['userpicture']
                 if eserpicture and allowed_file(eserpicture.filename):
                     filename = secure_filename(eserpicture.filename)
-                    eserpicture.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) 
+                    eserpicture.save(os.path.join(
+                        app.config['UPLOAD_FOLDER'], filename))
 
                     split_filename = filename.split(".")
                     split_filename[0] = esername
                     split_filename[1] = 'png'
                     newfilename = ('.'.join(split_filename))
 
-
-                    image = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                    image.thumbnail((320,320))
-                    image.save(os.path.join("static/userpictures",newfilename))
+                    image = Image.open(os.path.join(
+                        app.config['UPLOAD_FOLDER'], filename))
+                    image.thumbnail((320, 320))
+                    image.save(os.path.join(
+                        "static/userpictures", newfilename))
 
                     bucket = storage.bucket()
-                    blob = bucket.blob(os.path.join("userpictures/",newfilename))
-                    blob.upload_from_filename(os.path.join("static/userpictures",newfilename),content_type="image/png")
+                    blob = bucket.blob(os.path.join(
+                        "userpictures/", newfilename))
+                    blob.upload_from_filename(os.path.join(
+                        "static/userpictures", newfilename), content_type="image/png")
                     blob.make_public()
                     profile_url = (blob.public_url)
-                    logging.info(f"{esername} successfully uploaded a profile picture")
-
-
+                    logging.info(
+                        f"{esername} successfully uploaded a profile picture")
 
                 else:
                     session['message_minor'] = "Invalid profile picture was uploaded"
@@ -354,9 +350,7 @@ def register():
                 print(f"{esername} did not upload any photo")
                 logging.warning(f"{esername} did not uploaded any photo")
 
-
             print("profile_url : "+profile_url)
-
 
             ref = db.reference('/userAccounts')
             userAccounts = ref.get()
@@ -375,21 +369,22 @@ def register():
                             'ip': request.remote_addr
                         }
                     })
-                    session['message'] = "You're now registered " +esername+ " please login"
+                    session['message'] = "You're now registered " + \
+                        esername + " please login"
                     visitor_ip_address = request.remote_addr
-                    logging.info(f"REGISTRATION SUCCESS    username: {esername}   password: {eserpass}    IP Address: {visitor_ip_address}")
+                    logging.info(
+                        f"REGISTRATION SUCCESS    username: {esername}   password: {eserpass}    IP Address: {visitor_ip_address}")
                     return redirect(url_for('index'))
 
                 else:
                     error = "Passwords don't match, please repeat your password again"
                     return render_template("register.html", error=error)
-                    
+
             else:
                 error = "Username " + esername + " is already taken"
                 return render_template("register.html", error=error)
 
         return render_template("register.html")
-
 
 
 @app.route("/logout")
@@ -399,10 +394,9 @@ def logout():
     return redirect(url_for('index'))
 
 
-
 @app.errorhandler(404)
 def page_not_found(e):
-  return render_template('404.html'), 404
+    return render_template('404.html'), 404
 
 
 # Wrap Flask app with Talisman
